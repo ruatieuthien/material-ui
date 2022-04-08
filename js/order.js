a5e836710c5c701dec7199a2b7d61168889a4df7
Tuấn Anh Lê <letuananh06101997@gmail.com>
2022-04-08 21:29:08 +0700

    upcode material UI

---
+++ js/order.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        producNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {   productName: 'Ruko F11 Pro Drone',
+        producNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+     {   productName: 'Drone with Camera Drone',
+        producNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {   productName: 'GPS 4k Drone',
+        producNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+     {   productName: 'DJI Air 2S',
+        producNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+     {   productName: 'Lozenge Drone',
+        producNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
